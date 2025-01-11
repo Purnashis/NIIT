a66bc5d03c7eac622664db5854d3d5a2f04c216a
Purnashis <61218122+Purnashis@users.noreply.github.com>
2025-01-11 11:34:47 +0530

    update

--- efs-asg.sh
+++ efs-asg.sh
@@ -45,7 +45,7 @@ sudo systemctl enable mysqld
 EFS_DNS_NAME=fs-0010eb9b6121b2db3.efs.eu-west-2.amazonaws.com
 
 # mount the efs to the html directory 
-echo "$EFS_DNS_NAME:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
+echo "$EFS_DNS_NAME:/elevate-qa /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
 
 # set permissions
