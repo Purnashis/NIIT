3770b0b1e6f131027d71ee8d9f8933edf8d8f77a
Purnashis <61218122+Purnashis@users.noreply.github.com>
2025-01-12 15:44:21 +0530

    update

--- efs-asg.sh
+++ efs-asg.sh
@@ -4,9 +4,9 @@
 sudo yum update -y
 
 # install the apache web server, enable it to start on boot, and then start the server immediately
-sudo yum install -y git httpd
-sudo systemctl enable httpd 
-sudo systemctl start httpd
+sudo yum install -y git nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 # install php 8 along with several necessary extensions for wordpress to run
 sudo yum install -y \
@@ -30,19 +30,8 @@ php-openssl \
 php-pdo \
 php-tokenizer
 
-# Install Mysql-Client 
-sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm 
-sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
-sudo dnf repolist enabled | grep "mysql.*-community.*"
-sudo dnf install -y mysql-community-server 
-
-# start and enable the mysql server
-sudo systemctl start mysqld
-sudo systemctl enable mysqld
-
 # environment variable
-EFS_DNS_NAME=fs-0010eb9b6121b2db3.efs.eu-west-2.amazonaws.com
+EFS_DNS_NAME=xxxxxx
 
 # mount the efs to the html directory 
 echo "$EFS_DNS_NAME:/elevate-qa /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" | sudo tee -a /etc/fstab
@@ -53,7 +42,7 @@ sudo chown apache:apache -R /var/www/html
 sudo chmod 777 -R /var/www/html
 
 # restart the webserver
-sudo systemctl restart httpd
+sudo systemctl restart nginx
 sudo systemctl restart php-fpm
 
 # install code deploy agent
